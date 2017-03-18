cbc4b84f7316a3f588cc8632f082799a66df1297
Harun Kör <harunkor@gmail.com>
2017-03-19 00:22:02 +0200

    Initial commit

---
+++ app/src/main/java/com/harunkor/testbildirimornegi/MainActivity.java
@@ -0,0 +1,17 @@
+package com.harunkor.testbildirimornegi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.onesignal.OneSignal;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OneSignal.startInit(this).init();
+
+    }
+}
